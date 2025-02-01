7b5258b6db0f0d30c08982aac52fc07c773b2b48
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-02-01 16:01:50 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ConversorTemperaturaPadrao temperatura = new ConversorTemperaturaPadrao();
+        temperatura.celsiusParaFahrenheit(25);
+        temperatura.fahrenheitParaCelsius(75);
     }
 }
